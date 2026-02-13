b9fb05f19b4b84246c7ee451c038889489fada16
rpxexe <111rishwakarma@gmail.com>
2026-02-13 13:10:21 +0530

    payment using polar with better-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,16 +1,35 @@
+import {portal,checkout,polar} from "@polar-sh/better-auth"
 import { betterAuth } from "better-auth";
 import prisma from "@/lib/db"
-
+import { polarClient } from "./polar";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 
 
 export const auth = betterAuth({
-  database:prismaAdapter(prisma,{
-    provider:"postgresql"
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
   }),
-  emailAndPassword:{
+  emailAndPassword: {
     enabled: true,
-    autoSignIn:true
+    autoSignIn: true,
   },
-
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "c128b9a1-4bcb-474c-a5c5-246899803a99",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
